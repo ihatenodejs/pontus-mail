3658464bd9d438acd9a3893dc077525e954673af
Aidan <i@aidxn.fun>
2024-12-04 13:11:08 -0500

    disable form, use email submissions until form perfected

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const winston = require('winston');
-const rateLimit = require('express-rate-limit');
-const fs = require('fs');
+//const winston = require('winston');
+//const rateLimit = require('express-rate-limit');
+//const fs = require('fs');
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-    return `${timestamp} [${level.toUpperCase()}] ${message}`;
-    })
-  ),
-  transports: [
-    new winston.transports.File({ filename: 'register.log' })
-  ],
-});
+//const logger = winston.createLogger({
+//  level: 'info',
+//  format: winston.format.combine(
+//    winston.format.timestamp(),
+//    winston.format.printf(({ timestamp, level, message }) => {
+//    return `${timestamp} [${level.toUpperCase()}] ${message}`;
+//    })
+//  ),
+//  transports: [
+//    new winston.transports.File({ filename: 'register.log' })
+//  ],
+//});
 
-const exclusions = JSON.parse(fs.readFileSync('exclusions.json', 'utf8')).excludedIPs;
-
-const registerLimiter = rateLimit({
-  windowMs: 24 * 60 * 60 * 1000,
-  max: 1,
-  message: 'You have already submitted a registration today. Please try again tomorrow.',
-  standardHeaders: true,
-  legacyHeaders: false,
-  skip: (req, res) => exclusions.includes(req.ip)
-});
+//const exclusions = JSON.parse(fs.readFileSync('exclusions.json', 'utf8')).excludedIPs;
+//const registerLimiter = rateLimit({
+//  windowMs: 24 * 60 * 60 * 1000,
+//  max: 1,
+//  message: 'You have already submitted a registration today. Please try again tomorrow.',
+//  standardHeaders: true,
+//  legacyHeaders: false,
+//  skip: (req, res) => exclusions.includes(req.ip)
+//});
 
 const app = express();
 
@@ -42,19 +41,22 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.post('/register', registerLimiter, (req, res) => {
-  const formData = req.body;
-  logger.info(`New registration:
-    Name: ${formData.name}
-    Email: ${formData.email}
-    Reason: ${formData.message}
-    Telegram: ${formData.telegram}`);
-  res.render('success');
-});
+//app.post('/register', registerLimiter, (req, res) => {
+//  const formData = req.body;
+//  logger.info(`New registration:
+//    Name: ${formData.name}
+//    Email: ${formData.email}
+//    Reason: ${formData.message}
+//    Telegram: ${formData.telegram}`);
+//  res.render('success');
+//});
 
-app.get('/success', (req, res) => {
-  res.render('success');
-});
+// Shelved for now
+// Logic needs improvements
+
+//app.get('/success', (req, res) => {
+//  res.render('success');
+//});
 
 app.get('/register', (req, res) => {
   res.render('register');
