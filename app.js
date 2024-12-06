9a83064c0882eea9d32bbfd769acc00297d1a588
Aidan <i@aidxn.fun>
2024-12-06 15:05:38 -0500

    clean up unused pages, update package info from defaults

--- app.js
+++ app.js
@@ -2,31 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
-//const winston = require('winston');
-//const rateLimit = require('express-rate-limit');
-
-//const logger = winston.createLogger({
-//  level: 'info',
-//  format: winston.format.combine(
-//    winston.format.timestamp(),
-//    winston.format.printf(({ timestamp, level, message }) => {
-//    return `${timestamp} [${level.toUpperCase()}] ${message}`;
-//    })
-//  ),
-//  transports: [
-//    new winston.transports.File({ filename: 'register.log' })
-//  ],
-//});
-
-//const exclusions = JSON.parse(fs.readFileSync('exclusions.json', 'utf8')).excludedIPs;
-//const registerLimiter = rateLimit({
-//  windowMs: 24 * 60 * 60 * 1000,
-//  max: 1,
-//  message: 'You have already submitted a registration today. Please try again tomorrow.',
-//  standardHeaders: true,
-//  legacyHeaders: false,
-//  skip: (req, res) => exclusions.includes(req.ip)
-//});
 
 const app = express();
 
@@ -40,23 +15,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index', { currentPage: 'home' });
 });
-
-//app.post('/register', registerLimiter, (req, res) => {
-//  const formData = req.body;
-//  logger.info(`New registration:
-//    Name: ${formData.name}
-//    Email: ${formData.email}
-//    Reason: ${formData.message}
-//    Telegram: ${formData.telegram}`);
-//  res.render('success');
-//});
-
-// Shelved for now
-// Logic needs improvements
-
-//app.get('/success', (req, res) => {
-//  res.render('success');
-//});
 
 app.get('/services', (req, res) => {
   res.render('services', { currentPage: 'services' });
