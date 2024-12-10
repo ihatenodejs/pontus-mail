604541f9c402f36bc20fa8e21bc3afd8573955db
Aidan <aidan@p0ntus.com>
2024-12-09 21:02:29 -0500

    rm unused js

--- public/js/checkUsernameInput.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('email').addEventListener('input', function() {
-    let emailInput = document.getElementById('email').value;
-    let prefix = document.getElementById('email-prefix');
-    if (emailInput.includes('@p0ntus.com')) {
-        prefix.innerText = emailInput.split('@')[0];
-    } else {
-        prefix.innerText = emailInput;
-    }
-});
