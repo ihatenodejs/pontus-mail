2385d0bf7b5734add878fdce5a7e72bd270e4aa3
Aidan <aidan@p0ntus.com>
2024-12-20 01:16:45 -0500

    improve donation page, registration page, add button to request checker to home

---
+++ public/js/donate.js
@@ -0,0 +1,8 @@
+function copyToClipboard(elementId) {
+    const text = document.getElementById(elementId).innerText;
+    navigator.clipboard.writeText(text).then(() => {
+        alert('Copied to clipboard');
+    }).catch(err => {
+        console.error('Failed to copy: ', err);
+    });
+}
